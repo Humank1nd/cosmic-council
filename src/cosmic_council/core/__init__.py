e030ac5b679166e9c2ab8e423aaa1b41cee79d07
Humank1nd <mriamnotarobot@gmail.com>
2026-02-26 23:54:33 -0500

    Expose sahasrara core exports for recovered meta-layer

--- src/cosmic_council/core/__init__.py
+++ src/cosmic_council/core/__init__.py
@@ -4,6 +4,7 @@ from . import hexagon
 from . import cycles
 from . import reflection
 from . import explain
+from . import sahasrara
 
 # Core system modules - import only the working ones
 from . import core
@@ -25,12 +26,19 @@ from .hexagon import (
     EnterpriseResult
 )
 
+from .sahasrara import (
+    SahasraraEngine,
+    SahasraraState,
+    get_sahasrara_engine,
+)
+
 __all__ = [
     # Core modules
-    'hexagon', 'cycles', 'reflection', 'explain', 'core', 'types',
+    'hexagon', 'cycles', 'reflection', 'explain', 'core', 'types', 'sahasrara',
     
     # Key classes
     'CosmicCouncil', 'ProblemStatement', 'ProblemComplexity', 'EnterpriseType',
     'CycleStatus', 'EnhancedEnterpriseResult', 'CosmicCouncilHexagon',
-    'CycleResult', 'EnterpriseResult'
+    'CycleResult', 'EnterpriseResult',
+    'SahasraraEngine', 'SahasraraState', 'get_sahasrara_engine'
 ]
